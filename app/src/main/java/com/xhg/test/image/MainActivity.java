e1343dcf60648fce7e4c09bb73f5153c06003be8
xiong <18679554413@163.com>
2017-07-05 23:56:09 +0800

    Initial commit

---
+++ app/src/main/java/com/xhg/test/image/MainActivity.java
@@ -0,0 +1,13 @@
+package com.xhg.test.image;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
