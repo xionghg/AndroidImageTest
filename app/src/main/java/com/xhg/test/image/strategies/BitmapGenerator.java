03a5c8fd2cfef8c035f2486e1a65936118b071ca
honggang.xiong <honggang.xiong@yyf.com>
2018-04-04 11:12:06 +0800

    弃用AsyncTask，切换至rxjava的ParallelFlowable

--- app/src/main/java/com/xhg/test/image/strategies/BitmapGenerator.java
+++ app/src/main/java/com/xhg/test/image/strategies/BitmapGenerator.java
@@ -1,18 +1,16 @@
 package com.xhg.test.image.strategies;
 
 import android.graphics.Bitmap;
-import android.os.AsyncTask;
 
 import com.xhg.test.image.utils.Log;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import io.reactivex.Flowable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.parallel.ParallelFlowable;
 import io.reactivex.schedulers.Schedulers;
 
 /**
@@ -24,11 +22,8 @@ import io.reactivex.schedulers.Schedulers;
 
 public class BitmapGenerator {
     public static final int CPU_COUNT = Runtime.getRuntime().availableProcessors();
-    public static final boolean USE_RX_PARALLEL = false;
     private static final String TAG = "BitmapGenerator";
-    private static final AtomicInteger sIndex = new AtomicInteger(0);
     private final AtomicInteger mCurrentPoint = new AtomicInteger(0);
-    private MyAsyncTask mMyAsyncTask;
     private Disposable mDisposable;
     /**
      * 颜色数组，占用内存大，使用后回收
@@ -39,6 +34,7 @@ public class BitmapGenerator {
      */
     private volatile Status mStatus = Status.PENDING;
     private GeneratorParameter p;
+    private long startTime;
 
     private BitmapGenerator(GeneratorParameter parameter) {
         setParameter(parameter);
@@ -62,10 +58,6 @@ public class BitmapGenerator {
             return;
         }
         Log.d(TAG, "cancel is called");
-        if (mMyAsyncTask != null) {
-            mMyAsyncTask.cancel(false);
-            mMyAsyncTask = null;
-        }
         if (mDisposable != null) {
             if (!mDisposable.isDisposed()) {
                 mDisposable.dispose();
@@ -101,27 +93,22 @@ public class BitmapGenerator {
         mColorArray = new int[p.getTotal()];
         p.getCallback().onStart();
 
-        // TODO: 使用rxjava，但效率貌似不高
+        startTime = System.currentTimeMillis();
         mCurrentPoint.set(0);
         if (inParallel) {
-            if (USE_RX_PARALLEL) {
-                mDisposable = Flowable.range(0, p.getTotal())
-                        .parallel()
-                        .runOn(Schedulers.computation())
-                        .map(this::getPixelOnPosition)
-                        .filter(progress -> progress > 0)
-                        .sequential()
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe(this::updateProgress,
-                                e -> Log.e(TAG, "startGeneratePixel: ", e),
-                                this::generatePixelFinished);
-            } else {
-                mMyAsyncTask = new MyAsyncTask(inParallel, 0, p.getHeight());
-                mMyAsyncTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
-            }
+            ParallelFlowable<Integer> pFlowable = ParallelFlowable.from(Flowable.range(0, p.getHeight()), 2);
+            mDisposable = pFlowable
+                    .runOn(Schedulers.computation())
+                    .map(this::getPixelOnLine)
+                    .filter(progress -> progress > 0)
+                    .sequential()
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(this::updateProgress,
+                            e -> Log.e(TAG, "startGeneratePixel: ", e),
+                            this::generatePixelFinished);
         } else {
-            mDisposable = Flowable.range(0, p.getTotal())
-                    .map(this::getPixelOnPosition)
+            mDisposable = Flowable.range(0, p.getHeight())
+                    .map(this::getPixelOnLine)
                     .filter(progress -> progress > 0)
                     .subscribeOn(Schedulers.computation())
                     .observeOn(AndroidSchedulers.mainThread())
@@ -132,18 +119,17 @@ public class BitmapGenerator {
     }
 
     // return progress, -1 if not yet
-    private int getPixelOnPosition(int pos) {
-        final int x = pos % p.getWidth();
-        final int y = pos / p.getWidth();
-        // mColorArray[p.y * p.getWidth() + p.x] = mStrategy.getRGB(p.x, p.y);
-        mColorArray[pos] = p.getStrategy().getRGB(x, y);
+    private int getPixelOnLine(int line) {
+        for (int i = 0; i < p.getWidth(); i++) {
+            mColorArray[line * p.getWidth() + i] = p.getStrategy().getRGB(i, line);
+        }
 
         final int pre = mCurrentPoint.getAndIncrement();
         final int now = pre + 1;
-        int preProgress = pre * 100 / p.getTotal();
-        int nowProgress = now * 100 / p.getTotal();
+        int preProgress = pre * 100 / p.getHeight();
+        int nowProgress = now * 100 / p.getHeight();
         if (nowProgress > preProgress) {
-            Log.d(TAG, "getPixelOnPosition: progress=" + nowProgress);
+            Log.d(TAG, "getPixelOnLine: progress=" + nowProgress);
             return nowProgress;
         }
         return -1;
@@ -155,12 +141,12 @@ public class BitmapGenerator {
     }
 
     private void generatePixelFinished() {
-        Log.d(TAG, "generate pixel finished");
+        long cost = System.currentTimeMillis() - startTime;
+        Log.d(TAG, "generate pixel finished, cost: " + cost + "ms");
         mStatus = Status.FINISHED;
         Bitmap bitmap = Bitmap.createBitmap(mColorArray, p.getWidth(), p.getHeight(), Bitmap.Config.ARGB_8888);
         p.getStrategy().recycle(); // release after finished
         mColorArray = null;
-        mMyAsyncTask = null;
         mDisposable = null;
         p.getCallback().onBitmapCreated(bitmap);
     }
@@ -187,83 +173,6 @@ public class BitmapGenerator {
         void onBitmapCreated(Bitmap bitmap);
 
         default void onCanceled() {
-        }
-    }
-
-    static class Point {
-        int x;
-        int y;
-
-        Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-    private class MyAsyncTask extends AsyncTask<Integer, Integer, Boolean> {
-        String mName = "AsyncTask";
-        boolean mInParallel;
-        int mStartHeight;
-        int mEndHeight;
-        int mTotalPoint;
-        int mLastProgress = 0;
-
-        public MyAsyncTask(boolean inParallel, int start, int end) {
-            super();
-            mInParallel = inParallel;
-            mStartHeight = start;
-            mEndHeight = end;
-            mTotalPoint = (end - start) * p.getWidth();
-            mName = "MyAsyncTask@" + sIndex.incrementAndGet() + "-" + (inParallel ? "1" : "0");
-        }
-
-        @Override
-        protected Boolean doInBackground(Integer... params) {
-            Log.d(TAG, mName + " doInBackground begin");
-            // 单线程7.869s; 2线程4.001s; 3线程4.294s  3664 4405 4815;
-            // 6线程1.773s  1761 1871 1688; 7线程1.763s
-
-            List<Point> points = new ArrayList<>(mTotalPoint);
-            for (int y = mStartHeight; y < mEndHeight; y++) {
-                for (int x = 0; x < p.getWidth(); x++) {
-                    points.add(new Point(x, y));
-                }
-            }
-            if (mInParallel) {
-                points.parallelStream().forEach(this::getPixelOnPoint);
-            } else {
-                points.forEach(this::getPixelOnPoint);
-            }
-
-            Log.d(TAG, mName + " doInBackground end");
-            return true;
-        }
-
-        private void getPixelOnPoint(Point point) {
-            if (isCancelled()) {
-                return;
-            }
-            mColorArray[point.y * p.getWidth() + point.x] = p.getStrategy().getRGB(point.x, point.y);
-
-            int currentProgress = mCurrentPoint.incrementAndGet() * 100 / mTotalPoint;
-            if (mTotalPoint <= 100) {
-                publishProgress(currentProgress);
-            } else {
-                if (currentProgress > mLastProgress) {
-                    mLastProgress = currentProgress;
-                    publishProgress(mLastProgress);
-                }
-            }
-        }
-
-        @Override
-        protected void onProgressUpdate(Integer... values) {
-            updateProgress(values[0]);
-        }
-
-        @Override
-        protected void onPostExecute(Boolean success) {
-            generatePixelFinished();
         }
     }
 }
